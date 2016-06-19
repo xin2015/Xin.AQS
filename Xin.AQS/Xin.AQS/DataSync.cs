9edf2a5d7fb2c89f4f5b4fdee24a8f47c218bd64
Xin <564030525@qq.com>
2016-06-19 22:09:36 +0800

    WCF

--- Xin.AQS/Xin.AQS/DataSync.cs
+++ Xin.AQS/Xin.AQS/DataSync.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using ConsoleApplication.SyncAirMonitorPublishDataService;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
-using Xin.AQS.NationalAQIPublishService;
 using Xin.Basic;
 
 namespace Xin.AQS
@@ -24,14 +25,38 @@ namespace Xin.AQS
             return count > 0;
         }
 
-        public static void SyncNationalCityAQIPublishData(DateTime time)
+        public static void SyncNationalCityAQIPublishData(DateTime time, bool live = true)
         {
             string tableName = "NationalCityAQIPublishLive";
             if (!HasData(tableName, time))
             {
                 using (SyncServiceClient client = new SyncServiceClient())
                 {
-                    CityAQIPublishLive 
+                    List<CityAQIPublishLive> list;
+                    if (live)
+                    {
+                        list = client.SyncCityAQIPublishLive().Where(o => o.TimePoint == time).ToList();
+                    }
+                    else
+                    {
+                        list = client.SyncCityAQIPublishHistory(time, time).ToList();
+                    }
+                    if (list.Any())
+                    {
+                        DataTable dt = list.GetDataTable<CityAQIPublishLive>(tableName);
+                        try
+                        {
+                            SqlHelper.Insert(dt);
+                            dt.TableName = tableName.Replace("Live", "History");
+                            SqlHelper.Insert(dt);
+                            MissingData.DeleteMissingData(tableName, tableName, time);
+                        }
+                        catch (Exception e)
+                        {
+                            LogHelper.Logger.Error("SyncNationalCityAQIPublishData", e);
+                            MissingData.InsertOrUpateMissingData(tableName,tableName,time,e.Message);
+                        }
+                    }
                 }
             }
         }
