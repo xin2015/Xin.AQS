530704a91734358b682c96eb780fdddb8587c4cd
Xin <564030525@qq.com>
2016-06-19 21:38:44 +0800

    wrong

---
+++ Xin.AQS/Xin.AQS/DataHandle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Xin.AQS
+{
+    /// <summary>
+    /// 数据处理工具类
+    /// </summary>
+    public static class DataHandle
+    {
+        /// <summary>
+        /// 将小数值舍入到最接近的整数值
+        /// </summary>
+        /// <param name="d">要舍入的小数</param>
+        /// <returns></returns>
+        public static decimal? Round(decimal? d)
+        {
+            decimal? result;
+            if (d.HasValue)
+            {
+                result = Math.Round(d.Value);
+            }
+            else
+            {
+                result = d;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 将小数值按指定的小数位数舍入
+        /// </summary>
+        /// <param name="d">要舍入的小数</param>
+        /// <param name="decimals">返回值中的小数位数</param>
+        /// <returns></returns>
+        public static decimal? Round(decimal? d, int decimals)
+        {
+            decimal? result;
+            if (d.HasValue)
+            {
+                result = Math.Round(d.Value, decimals);
+            }
+            else
+            {
+                result = d;
+            }
+            return result;
+        }
+    }
+}
---
+++ Xin.AQS/Xin.AQS/DataSync.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using Xin.AQS.NationalAQIPublishService;
+using Xin.Basic;
+
+namespace Xin.AQS
+{
+    public class DataSync
+    {
+        private static string selectCountSqlString;
+        static DataSync()
+        {
+            selectCountSqlString = "select count(*) [Count] from {0} where Time = @Time";
+
+        }
+        public static bool HasData(string tableName, DateTime time)
+        {
+            string cmdText = string.Format(selectCountSqlString, tableName);
+            SqlParameter parameter = new SqlParameter("@Time", time);
+            int count = (int)SqlHelper.ExecuteScalar(cmdText, parameter);
+            return count > 0;
+        }
+
+        public static void SyncNationalCityAQIPublishData(DateTime time)
+        {
+            string tableName = "NationalCityAQIPublishLive";
+            if (!HasData(tableName, time))
+            {
+                using (SyncServiceClient client = new SyncServiceClient())
+                {
+                    CityAQIPublishLive 
+                }
+            }
+        }
+    }
+}
